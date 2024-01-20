5ccf590d761e9c788d30602aa0e213ff18b1a167
Pixiuchu <pixiuchu@gmail.com>
2024-01-20 01:52:43 +0100

    now automatically parses fmapdat from cia file

--- FMapDat.py
+++ FMapDat.py
@@ -2,7 +2,13 @@ import random
 import shutil
 import FMapDat_lists as fmd
 
-shutil.copyfile("FMapDat-ReadOnly/FMapDat.dat", "FMapDat-Randomized/FMapDat.dat")
+with open("00040000000D9000_v00.standard.cia", mode="rb") as f:
+    ### Copy FMapDat.dat from .cxi
+    f.seek(0x07636F60)
+    copyfmapdat = f.read(0x14CFF000)
+    fmdfile = open("FMapDat-Randomized/FMapDat.dat", mode="w+b")
+    fmdfile.write(copyfmapdat)
+
 data = fmd.Locations
 shuffled = list(data.values())
 random.shuffle(shuffled)
@@ -10,7 +16,9 @@ x = dict(zip(data, shuffled))
 z = list(x.values())
 listaddress = list(x.keys())
 
-print(listaddress[1])
+#print(listaddress[1])
+### Randomize the blocks --- no beans, no sneeze-blocks: they need their own checking so that they don't become blocks.
+#### Logic needs to be added: Luigi blocks should not be in Mini Mario areas, these are inaccessible without glitches.
 with open("FMapDat-Randomized/FMapDat.dat", mode="r+b") as f:
     i = 0
     while i < 632:
